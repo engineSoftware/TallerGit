0021012dcc4dc56b2e0d42c74d46095a9077297d
engineSoftware <engineSoftware>
2018-07-03 12:21:21 -0500

    Primera version

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+package estructurados;
+
+public class HolaMundo
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hola taller");
+		new java.util.Scanner(System.in).nextLine();
+	}
+
+}
