f2075af4b920d82fd347c1ba41306856d20bee07
Daniel Godoy <36893895+engineSoftware@users.noreply.github.com>
2019-03-29 10:25:53 -0600

    Tercer commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,8 +6,6 @@ public class HolaMundo
 	public static void main(String[] args)
 	{
 		System.out.println("Hola taller");
-		System.out.println("Primer cambio");
-		System.out.println("Segundo cambio");
 		new java.util.Scanner(System.in).nextLine();
 	}
 
